4806c6a64a8f3c21e4b299f803dc48b497826e06
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-03-21 21:29:06 +0100

    added full status messsage support

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const game = require("./utils/PlayerData/steamid");
 const faceit = require("./utils/PlayerData/faceit");
 const mm = require("./utils/PlayerData/mm");
 const example = require("./utils/example");
-const message = require("./utils/Messages/messageBuilder");
 const RETRY_TIMEOUT = 10 * 1000;
 
 async function listen() {
@@ -15,7 +14,6 @@ async function listen() {
     negotiationMandatory: false,
     timeout: 1500,
   };
-
   try {
     await connection.connect(params);
   } catch (e) {
@@ -28,51 +26,107 @@ async function listen() {
     return;
   }
   const socket = connection.getSocket();
+  var current_cmd = "";
+  var msg_log = "";
   socket.on("data", async (data) => {
     var msg = data.toString("utf8");
-    if (msg.includes("STEAM_") ) {
+    if (msg.match(/players : ([\d.]+) humans, /) && current_cmd != "" ){
+      var players = msg.match(/([\d.]+) *humans/)[1];
+      msg_log = msg_log + msg;
+      console.log(players);
+    }
+    if (msg.includes("STEAM_") && current_cmd != "") {
       if(process.env.NODE_ENV.trim() !== "production"){
        var msg = example.statusmessage3;
       }else {
-        var msg = data.toString("utf8");
+         msg_log = msg_log + msg;
+        console.log(msg_log);
+        console.log("--------------")
       }
+      if (msg.includes("#end")){
+        console.log("end read");
+        
+      
       let playerList = [];
-      ids = game.getSteamIds(msg);
+      ids = game.getSteamIds(msg_log);
       let url = mm.createUrl(ids);
-      let steamusernames = game.getSteamUsername(msg);
+      let steamusernames = game.getSteamUsername(msg_log);
       let players = await mm.getMMRank(url, ids, steamusernames);
        faceit.getElo(players).then((newPlayers) => {
         for (let i = 0; i < newPlayers.length; i++) {
           playerList.push(newPlayers[i]);
         } 
-        initMessage(playerList, connection);
+
+        if (current_cmd == "getelo"){
+          //console.log(playerList.length);
+          consoleMessage(playerList, connection);
+          current_cmd="";
+          msg_log = "";
+        }
+        if (current_cmd == "printelo"){
+          printMessage(playerList, connection);
+          current_cmd="";
+          msg_log = "";
+        }
+
+       
+
        }).catch((err) => {});
+       
+      }
     }else if(msg.includes("getelo")) {
       try {
+        current_cmd = "getelo";
         await connection.exec("status");
       } catch (e) {
         console.log("error in status");
       }
     } 
+    else if(msg.includes("printelo")) {
+      try {
+        current_cmd = "printelo";
+        await connection.exec("status");
+      } catch (e) {
+        console.log("error in status");
+      }
+    } 
+   
   });
 }
 
-async function initMessage(playerList, con) {
+async function consoleMessage(playerList, con) {
+  var message = "";
     for (let i = 0; i < playerList.length; i++) {
-      await sendMessage(con, playerList[i]);
-      setTimeout(() => {},550);
+      message= message + `echo ${playerList[i].steamusername}, MM-Rank: ${
+        playerList[i].mmRank
+      } / Faceit: ${
+        playerList[i].elo == "no elo" ? "No Acc found" : playerList[i].elo
+      } \n`
+      //await sendMessage(con, playerList[i]);
+      //setTimeout(() => {},1000);
     }
-   try {
-    await con.exec("say visit github.com/bonbonn1912/ConsoleEloCsgo for more information");
-   }catch(e){}
+    sendMessage(con,0, message);
+    //sendMessage(con,1000, "echo visit github.com/bonbonn1912/ConsoleEloCsgo for more information");
 
 }
 
-async function sendMessage(con, singlePlayer) {
+async function printMessage(playerList, con) {
+    for (let i = 0; i < playerList.length; i++) {
+      
+      await sendMessage(con,1000, `say ${playerList[i].steamusername}, MM-Rank: ${
+        playerList[i].mmRank
+      } / Faceit: ${
+        playerList[i].elo == "no elo" ? "No Acc found" : playerList[i].elo
+      }`);
+    }
+    //sendMessage(con,1000, "say visit github.com/bonbonn1912/ConsoleEloCsgo for more information");
+
+}
+
+async function sendMessage(con,delay,  message) {
   try {
-    await con.exec(
-      `${message.buildOutput(singlePlayer, "tsay", "full")}`
-    );
+    await con.exec(message);
+    setTimeout(() => {},delay);
   } catch (e) {}
 }
 
